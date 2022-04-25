88e8e7ddb95b5cd2f17cb520ccdba15f693cc03d
yaoleksa <mryaremchyk@gmail.com>
2022-04-25 16:35:55 +0300

    change endpoint

--- a.js
+++ a.js
@@ -1,8 +1,9 @@
 const request = new XMLHttpRequest();
 
-request.open('POST', 'http://localhost:3000');
+request.open('POST', 'https://yaoleksa.github.io/sample_server/');
 request.send('badly');
 
 request.onload = () => {
     console.log(request.status);
+    console.log('not');
 }
